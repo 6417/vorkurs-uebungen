5b661eae9619ad7e59b0c16fcb430f29d373a9b9
Ambros <ambros.anrig@gmail.com>
2025-11-19 18:57:56 +0100

    Added call of Control()

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.PrintVorkursCommand;
@@ -14,6 +13,9 @@ public class Controls implements Sendable {
     private Trigger xbutton = joystick.x();
     private Trigger abutton = joystick.a();
     private Trigger bbutton = joystick.b();
+  
+    private final CommandXboxController m_driverController =
+      new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
 
     public Controls() {
         joystick = new CommandXboxController(Constants.Controls.xboxID);
